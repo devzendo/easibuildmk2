d9039b6f3aba54bcd4e7e8ecf1a707027777358f
Matt Gumbley <matt.gumbley@devzendo.org>
2015-09-29 08:15:02 +0100

    Start splitting code out from the main .ino file into reusable modules, for use in the hardware test program.

--- EasiBuildMk2.ino
+++ EasiBuildMk2.ino
@@ -24,29 +24,8 @@
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
 
-// CHANGE_PINS
-// Right-hand side of Arduino Micro...
-// Input pins are all on PINB for rapid reading in the pin change interrupt
-// service routine.
-const int dah_in = 12;
-const int dit_in = 11;
-const int encr_in = 10;
-const int encl_in = 9;
-const int btn_in = 8;
-const int band1_out = 7;
-const int rxtx_out = 6;
-const int sidetone_out = 5; // PWM, with RC low-pass filter network to convert
-                            // to analogue. On OCR3A, PWM phase correct.
-const int key_out = 4;
-// display is on 2 and 3, i2c.
+#include "PinAssignments.h"
 
-// Left-hand side of Arduino Micro...
-const int band2_out = 19;
-const int band3_out = 18;
-const int dds_clock_out = 14;
-const int dds_update_out = 15;
-const int dds_data_out = 16;
-const int dds_reset_out = 17;
 
 //==============================================================================
 //=== SIDETONE OSCILLATOR (Thanks to Martin Nawrath)
