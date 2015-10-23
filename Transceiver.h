a035b02c0ce6a41da20f02bf930527b8a025643b
Matt Gumbley <matt.gumbley@devzendo.org>
2015-10-23 06:39:47 +0100

    A work in progress, thinking out loud, scratching.....

--- Transceiver.h
+++ Transceiver.h
@@ -5,130 +5,171 @@
 #ifndef _TRANSCEIVER_H_
 #define _TRANSCEIVER_H_
 
-//==============================================================================
-//=== PIN CHANGE INTERRUPT
-//==============================================================================
+#include "Menu.h"
 
-/*
- *  value = portb & interesting_bits;
- *  if (value == last_portb) {
- *    return;
- *  }
- *
- *  last_portb = value;
- *  this_dah = value & dah;
- *  if (this_dah != last_dah) {
- *    last_dah = this_dah;
- *    raise(dah_event, this_dah ? HIGH : LOW);
- *  }
- *  this_dit = value & dit;
- *  if (this_dit != last_dit) {
- *    last_dit = this_dit;
- *    raise(dit_event, this_dit ? HIGH : LOW);
- *  }
- *  this_btn = value & btn;
- *  if (this_btn != last_btn) {
- *    last_btn = this_btn;
- *    raise(btn_event, this_btn ? HIGH : LOW);
- *  }
- *
- *  // do rotary encoder
- *
- */
+class NumberedList immediateMenu = {
+   8, 
+   {
+       "Freq. lock", 
+       "R.I.T.",
+       "Send macro 1",
+       "Send macro 2",
+       "Send macro 3",
+       "Send macro 4",
+       "Send macro 5",
+       "Tune",
+   }
+};
+
+class NumberedList setupMenu = {
+    8,
+    {
+        "Filter 1",
+        "Filter 2",
+        "Filter 3",
+        "CW delay",
+       //1234567890123456  
+        "CW filt. freq.",
+        "CW key type",
+        "Farnsworth spd.",
+        "Character spd.",
+        "Paddle polarity",
+        "Edit macro 1",
+        "Edit macro 2",
+        "Edit macro 3",
+        "Edit macro 4",
+        "Edit macro 5",
+        "Reset settings",
+        "Sidetone pitch",
+       //1234567890123456  
+        "Sidetone volume",
+        "Version"
+    }
+};
+
+const int modeOperator = 0;
+const int modeImmediate = 1;
+const int modeSetup = 2;
+volatile int mode;
+
+volatile double ddsFrequency;
+double ddsDigitDelta;
+double lowerBandBound;
+double upperBandBound;
+
+void refreshOperator() {
+}
+
+void refreshImmediate() {
+}
+
+void refreshSetup() {
+}
+
+
+void refresh() {
+    switch (mode) {
+        case modeOperator: refreshOperator(); break;
+        case modeImmediate: refreshImmediate(); break;
+        case modeSetup: refreshSetup(); break;
+    }
+}
+
+void enterOperator() {
+}
+
+void enterImmediate() {
+}
+
+void enterSetup() {
+}
+
+void switchMode(int newMode) {
+    mode = newMode;
+    switch (mode) {
+        case modeOperator: enterOperator(); break;
+        case modeImmediate: enterImmediate(); break;
+        case modeSetup: enterSetup(); break;
+    }
+    refresh();
+}
+
+void eventOperator(int event) {
+//    if (event & evLeft) {
+        
+}
+
+void eventImmediate(int event) {
+}
+
+void eventSetup(int event) {
+}
 
 //==============================================================================
 //=== SCOOP TASKS
 //==============================================================================
 
-// user definition of task1 object
-defineTask(Task1)
-// force any read/write to memory
-volatile long count;
-
-void Task1::setup() {
-    count = 0;
-};
-
-void Task1::loop() {
-    sleepSync(1000);
-    count++;
-};
-
-
-// user quick definition of task2 object 
-defineTaskLoop(Task2)
-{   digitalWrite(13, HIGH);
-    sleep(100);
-    digitalWrite(13, LOW);
-    sleep(100);
+defineTaskLoop(Task1) {
+    int event;
+    if (eventFifo.get(&event)) {
+        switch (mode) {
+            case modeOperator: eventOperator(event); break;
+            case modeImmediate: eventImmediate(event); break;
+            case modeSetup: eventSetup(event); break;
+        }
+    }
+    yield();
 }
 
 //==============================================================================
 //=== MAIN SETUP AND LOOP
 //==============================================================================
 
-// main setup
-void setup() {
+char *version="0.0"; // only ever x.x, more requires rework below...
+
+// Also called by factory reset
+void initialise() {
+    char buf[20];
     setupPins();
-    setupDisplay();
-    
     setupSidetoneTimer3();
 
+    lcd.setCursor(0, 0);
             // 0123456789ABCDEF
-    lcd.print("seconds spent :");
-    Serial.begin(115200);
-    Serial.print("seconds spent :");
+    lcd.print("EasiBuild Mk 2  ");
+    lcd.setCursor(0, 1);
+    sprintf(buf, "v%s, M0CUV  ", version);
+    lcd.print(buf);
     
-    mySCoop.start();
+    // start timer for 1sec to switch to next mode
+    // check nvram checksum
+    // if wrong, reset to sane, compute checksum, save, set to goto setup mode
+    // if ok, set to goto operator mode
+    // load all runtime settings from nvram area
+    // if filters set up
+    //    set up band filter switch
+    // if frequency set up
+    //    set up dds
+    ddsFrequency = 0.0;
+    ddsDigitDelta = 1.0; // TODO store 
+    lowerBandBound = 14000000.0;
+    upperBandBound = 14350000.0;
+    // goto determined mode
+    switchMode(modeOperator);
 }
 
-// main loop
-bool oldtransmit = true; // display fostamin    
-char ch = 'x';
-int oldstate = 99; // display fostamin
+// main setup
+
+void setup() {
+    setupDisplay();
+    Serial.begin(115200);
+    mySCoop.start();
+
+    initialise();
+}
+
 void loop() {
     yield();
-
-    if (digitalRead(dit_in) == LOW) {
-        transmit = true;
-    }
-    if (digitalRead(dah_in) == LOW) {
-        transmit = false;
-    }
-
-    if (oldtransmit != transmit) {
-        lcd.setCursor(0, 1);
-        lcd.print(transmit ? "*" : "O");
-        oldtransmit = transmit;
-    }
-    
-    if (oldstate != txState) {
-        switch (txState) {
-            case TXS_IDLE_SILENT:
-                ch = 'i';
-                break;
-            case TXS_ENABLE_RAMPUP:
-                ch = 'u';
-                break;
-            case TXS_ON:
-                ch = 'o';
-                break;
-            case TXS_DISABLE_RAMPDOWN:
-                ch = 'd';
-                break;
-        }
-        lcd.setCursor(1, 1);
-        lcd.print(ch);
-        lcd.print(' ');
-        lcd.print(rampCycles);
-        lcd.print(' ');
-        lcd.print(volume);
-        lcd.print(' ');
-        oldstate = txState;
-    }    
 }
-
-
 
 #endif
 
